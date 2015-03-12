8ceecbaea28d4b29246e63dbd64f12bf2cfbfaf2
ilhwan Cho <joilhown@naver.com>
2015-03-13 11:54:45 +1300

    index page layout

--- header.php
+++ header.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
+	<title>Information of Otago</title>
     <meta charset="utf-8" />    
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- css -->
 	<link rel="stylesheet" type="text/css" href="css/index.css">
-
+	<!-- <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet"> -->
+	<!-- jQuery CDN -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- <script src="bootstrap/js/bootstrap.min.js"></script>-->
  </head>
 
  <body>
--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 require_once("header.php");
 ?>
 <!-- Top Navigation -->
-<div class="top_navi"></div>
+<div class="top_navi">
+	<div class="logo">
+	<a href="#"><img src="img/logo/NZ_flag.png" alt="New Zealnd Flag"></a>
+	</div>
+
+	<div class="navigation">
+		<ul>			
+			<li class="nav"><a href="#">Information</a></li>
+			<!-- <li class="nav"><a href="#">Visit</a></li>
+			<li class="nav"><a href="#">Accomodation</a></li>
+			<li class="nav"><a href="#">Restuarant</a></li>
+			<li class="nav"><a href="#">Event</a></li> -->
+			<li class="nav"><a href="#">Location</a></li>
+		</ul>
+	</div>
+
+</div>
 
 <!-- Main Images -->
 <div class="main_images"></div>
@@ -13,5 +29,9 @@ require_once("footer.php");
 
 
 
-
+<li>Dunedin</li>
+<li>Queenstown</li>
+<li>North Otago</li>
+<li>The Catlins</li>
+<li>Central Otago</li>
  
