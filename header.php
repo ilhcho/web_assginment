e25d026dd1c1f8a0dad9ba1b3a38f7af6409c3a9
ilhwan Cho <joilhown@naver.com>
2015-03-12 23:24:06 +1300

    Web_Assignment - index page layout

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />    
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- css -->
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+
+ </head>
+
+ <body>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require_once("header.php");
+?>
+<!-- Top Navigation -->
+<div class="top_navi"></div>
+
+<!-- Main Images -->
+<div class="main_images"></div>
+
+<?php
+require_once("footer.php");
+?>
+
+
+
+
+ 
