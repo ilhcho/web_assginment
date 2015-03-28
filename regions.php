acea65a25bd0487767ef34f094384010c7ca6b17
ilhwan Cho <joilhown@naver.com>
2015-03-28 22:09:39 +1300

    Central Otago: Accomodation

--- regions.php
+++ regions.php
@@ -33,16 +33,34 @@ require_once("regions_navi.php");
 
 <div class="middle middle_box">
   <div class="container container_middle">
-   <a id="accomodation"> <div class="infor_box">
-    <h3>Accomodation</h3>
-
-    Stay in a luxury lodge with gourmet meals and stunning vistas of snow-dusted mountain ranges, big skies and empty spaces. Or enjoy the comradeship that comes with sharing facilities and yarns with other travellers at a backpacker lodge. When it comes to booking accommodation for your travel to Central Otago, there is also plenty in-between – bed and breakfast, farmstay, homestay, self contained, holiday home, hotel, motel and holiday park. 
-
-Central Otago’s commercial centres at Alexandra and Cromwell offer the most plentiful and widest variety of accommodation types. They have a particularly broad range of reasonably priced and higher-end motels and hotels; and budget holiday park and backpacker accommodation. 
-
-The small townships spread out across New Zealand's Central Otago have limited accommodation options and they can fill up quickly. Most have at least one hotel. Sometimes these will be restored, historic buildings. Most towns will also offer other Central Otago accommodation types, particularly self-contained and homestay. 
-
-Farmstays are likely to be in remote areas, possibly well away from towns. They offer a refuge and a glimpse into New Zealand farm life. Locally raised lamb or beef will almost certainly be on the menu, too. 
+   <a id="accomodation"> 
+    <div class="infor_box">
+      <h3>Accomodation</h3>
+      <div class="acco_box">
+        <img class="acco_img" src="img/accomodation/central/holiday.jpg" alt="central otago holiday house">
+          <div class="aaco_sub">
+          <h2>Holiday House</h2>
+          <h4>Price Range: $75~$200</h4>
+          <h4>Feature:</h4>
+          <h4>Web Site:<a href="https://www.holidayhouses.co.nz/Alexandra.asp" target="_Blank"> Holiday Houses</a></h4>
+          </div>
+      </div>
+      <div class="acco_box"> <img class="acco_img" src="img/accomodation/central/hotel.jpg" alt="central otago holiday house">
+          <div class="aaco_sub">
+          <h2>Luxury Hotel</h2>
+          <h4>Price Range: $129 ~ $449</h4>
+          <h4>Feature:           
+          <h4>Web Site:<a href="http://www.aatravel.co.nz/new-zealand/Central-Otago-District_Hotels.html" target="_Blank"> Hotels</a></h4>
+          </div>
+      </div>
+      <div class="acco_box"> <img class="acco_img" src="img/accomodation/central/backpack.jpg" alt="central otago holiday house">
+          <div class="aaco_sub">
+          <h2>Backpackers & Holiday Park</h2>
+          <h4>Price Range: $13 ~ $120</h4>
+          <h4>Feature:</h4>
+          <h4>Web Site: <a href="http://www.tourism.net.nz/region/otago/otago---central-otago/accommodation/backpacker-and-budget" target="_Blank">Backpackers</a></h4>
+          </div>
+      </div>    
 
     </div></a>
 
