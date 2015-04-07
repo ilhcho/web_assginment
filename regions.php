4332eec6d494adf38483fa8498313ecde16a1b5f
ilhwan Cho <joilhown@naver.com>
2015-04-07 21:42:10 +1200

    Central Otago: 1) Calendar Demo  2) Resuarants

--- regions.php
+++ regions.php
@@ -157,8 +157,8 @@ require_once("regions_navi.php");
             </div>
             <a href="http://www.winesofnz.com/Regions.aspx?region=otago&AspxAutoDetectCookieSupport=1" class="button2" target="_Blank">Visit the website</a>
             </div>
-          </div>
-         
+              </div>
+     
           <div class="span4">
             <div class="att_des_side">
               <div class="header">ROXBURGH</div>
@@ -187,28 +187,110 @@ require_once("regions_navi.php");
   </a>
 
     <a id="event"><div class="infor_box">
-    <h3>Event</h3>
-    Stay in a luxury lodge with gourmet meals and stunning vistas of snow-dusted mountain ranges, big skies and empty spaces. Or enjoy the comradeship that comes with sharing facilities and yarns with other travellers at a backpacker lodge. When it comes to booking accommodation for your travel to Central Otago, there is also plenty in-between – bed and breakfast, farmstay, homestay, self contained, holiday home, hotel, motel and holiday park. 
+    <a href="http://www.centralotagonz.com/event-calendar" target="_Blank"><h3>Event</h3></a>
+    <div class="event_calender">
+    <div class="spring">
+      <div class="months border_right months_9"><div class="vertical_center">September</div></div>
+      <div class="months border_right months_10"><div class="vertical_center">October</div></div>
+      <div class="months months_11"><div class="vertical_center">November</div></div>
+    </div>
+    <div class="summer">
+      <div class="months border_right months_12"><div class="vertical_center">December</div></div>
+      <div class="months border_right months_1"><div class="vertical_center">January</div></div>
+      <div class="months months_2"><div class="vertical_center">February</div></div>
+    </div>
+    <div class="autumn">
+      <div class="months border_right months_3"><div class="vertical_center">March</div></div>
+      <div class="months border_right months_4"><div class="vertical_center">April</div></div>
+      <div class="months months_5"><div class="vertical_center">May</div></div>
+    </div>
+    <div class="winter">
+      <div class="months border_right months_6"><div class="vertical_center">June</div></div>
+      <div class="months border_right months_7"><div class="vertical_center">July</div></div>
+      <div class="months months_8"><div class="vertical_center">August</div></div>
+    </div>
 
-Central Otago’s commercial centres at Alexandra and Cromwell offer the most plentiful and widest variety of accommodation types. They have a particularly broad range of reasonably priced and higher-end motels and hotels; and budget holiday park and backpacker accommodation. 
-
-The small townships spread out across New Zealand's Central Otago have limited accommodation options and they can fill up quickly. Most have at least one hotel. Sometimes these will be restored, historic buildings. Most towns will also offer other Central Otago accommodation types, particularly self-contained and homestay. 
-
-Farmstays are likely to be in remote areas, possibly well away from towns. They offer a refuge and a glimpse into New Zealand farm life. Locally raised lamb or beef will almost certainly be on the menu, too. 
+    </div><!--End of event_calender-->
 
     </div></a>
 
    <a id="restaurant"><div class="infor_box">
     <h3>Restaurant</h3>
-    Stay in a luxury lodge with gourmet meals and stunning vistas of snow-dusted mountain ranges, big skies and empty spaces. Or enjoy the comradeship that comes with sharing facilities and yarns with other travellers at a backpacker lodge. When it comes to booking accommodation for your travel to Central Otago, there is also plenty in-between – bed and breakfast, farmstay, homestay, self contained, holiday home, hotel, motel and holiday park. 
+    <div class="res_wrapper">
+    <div class="img_part">
+      <div class="res_img">
+        <img class="img_radius" src="img/restaurant/central/cellar.jpg" alt="CELLAR DOORS">
+     </div>
+    </div>
+    <div class="des_part">
+    <div class="des_part2">
+      <a href="http://www.centralotagonz.com/New-Zealand/Cellar-Door_sbt_arm=0_stm=0_st=126_.html"><h5>Cellar Doors</h5></a>
+      <h4 id="res_des">Most Central Otago wineries offer visitors wine tasting and the opportunity to buy their wines.</h4>
+      <h4 id="res_des"> It is more personal than going to a store or restaurant.</h4>
+      <ul class="a">
+       <li><a href="http://www.mtdifficulty.co.nz" target="_Blank">Mt Difficulty Wines</a></li>
+       <li><a href="http://www.wooingtree.co.nz" target="_Blank">Wooing Tree Vineyard</a></li>
+       <li><a href="http://www.carrick.co.nz" target="_Blank">Carrick Winery Restaurant & Cellar Door</a></li>
+       <li><a href="http://www.pinotthymetours.co.nz" target="_Blank">Pinot Thyme Wine Tours</a></li>   
+      </ul>
+      <h4><a href="http://www.centralotagonz.com/New-Zealand/Cellar-Door_sbt_arm=0_stm=0_st=126_.html" target="_Blank">Read More</a></h4>
+    
+    </div>
+    </div>
 
-Central Otago’s commercial centres at Alexandra and Cromwell offer the most plentiful and widest variety of accommodation types. They have a particularly broad range of reasonably priced and higher-end motels and hotels; and budget holiday park and backpacker accommodation. 
+    </div>
+    <hr class="divider">
 
-The small townships spread out across New Zealand's Central Otago have limited accommodation options and they can fill up quickly. Most have at least one hotel. Sometimes these will be restored, historic buildings. Most towns will also offer other Central Otago accommodation types, particularly self-contained and homestay. 
+<div class="res_wrapper">
+    <div class="img_part">
+      <div class="res_img">
+        <img class="img_radius" src="img/restaurant/central/restaurants.jpg" alt="CENTRAL OTAGO RESTAURANTS">
+     </div>
+    </div>
+    <div class="des_part">
+    <div class="des_part2">
+      <a href="http://www.centralotagonz.com/New-Zealand/Restaurants_sbt_arm=0_stm=0_st=100_.html"><h5>Central Otago Restaurants</h5></a>
+      <h4 id="res_des">Restaurants are most stock award-winning Central Otago wines.</h4>
+      <h4 id="res_des">Restaurants offer fine dining in the evenings.</h4>
+      <ul class="a">
+       <li><a href="http://www.goldengate.co.nz" target="_Blank">Golden Gate Lodge</a></li>
+       <li><a href="http://www.lakeroxburghlodge.co.nz" target="_Blank">Lake Roxburgh Lodge</a></li>
+       <li><a href="http://www.centralotagonz.com/cromwell-bannockburn/pisa-moorings/restaurants/x,1,5275/the-moorings-restaurant-bar.html" target="_Blank">The Moorings Restaurant & Bar</a></li>
+       <li><a href="http://www.mtdifficulty.co.nz" target="_Blank">Mt Difficulty Wines</a></li>   
+      </ul>
+      <h4><a href="http://www.centralotagonz.com/New-Zealand/Restaurants_sbt_arm=0_stm=0_st=100_.html" target="_Blank">Read More</a></h4>
+    
+    </div>
+    </div>
 
-Farmstays are likely to be in remote areas, possibly well away from towns. They offer a refuge and a glimpse into New Zealand farm life. Locally raised lamb or beef will almost certainly be on the menu, too. 
-<br />
-<br />
+    </div>
+    <hr class="divider">
+
+    <div class="res_wrapper">
+    <div class="img_part">
+      <div class="res_img">
+        <img class="img_radius" src="img/restaurant/central/cafe.jpg" alt="CAFÉS IN CENTRAL OTAGO">
+     </div>
+    </div>
+    <div class="des_part">
+    <div class="des_part2">
+      <a href="http://www.centralotagonz.com/New-Zealand/Caf%C3%A9_sbt_arm=0_stm=0_st=153_.html"><h5>Cafés in Central Otago</h5></a>
+      <h4 id="res_des">Cafés have a relaxed atmosphere and usually offer a casual all-day dining 
+      experience. </h4>
+      <h4 id="res_des"> Most cafés provide juices, teas, espresso coffee and 
+      other hot and cold drinks.</h4>
+       <ul class="a">
+       <li><a href="http://www.wildearthwines.co.nz" target="_Blank">Wild Earth</a></li>
+       <li><a href="http://www.centralotagonz.com/roxburgh/roxburgh/cafes/x,1,5244/rox-cafe.html" target="_Blank">Rox Cafe</a></li>
+       <li><a href="http://www.speargrassinn.co.nz" target="_Blank">Speargrass Inn</a></li>
+       <li><a href="http://www.centralotagonz.com/cromwell-bannockburn/bannockburn/restaurants/x,1,5283/the-kitchen-bannockburn.html" target="_Blank">The Kitchen Bannockburn</a></li>   
+      </ul>
+      <h4><a href="http://www.centralotagonz.com/New-Zealand/Caf%C3%A9_sbt_arm=0_stm=0_st=153_.html" target="_Blank">Read More</a></h4>
+    
+    </div>
+    </div>
+    </div>
+
 
     </div></a>
 
